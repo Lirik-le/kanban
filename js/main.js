c67cb67ed11fe116a833be6824a52dfde7ce19be
gr402_lekev <kirill.lebed.95@mail.ru>
2023-02-03 13:48:19 +0700

    :(

--- js/main.js
+++ js/main.js
@@ -5,10 +5,35 @@ Vue.component('kanban', {
         <div class="todolist">
             <div class="formCard">
                 <h1>Kanban-доска</h1>
-                <div v-show="addCard" class="modalBackgrVis">
+                <div v-show="modal" class="modalBackgrVis">
                     <add-note class="modalWindow" :changeModal="changeModal"></add-note>
                 </div>
                 <input class="btn" type="button" value="Добавить задачу" @click="changeModal">
+
+
+                <div v-show="changeCard" class="modalBackgrVis">
+                    <form @submit.prevent="" class="modalWindow">
+                        <h1 class="cross" @click="changeModalCard">+</h1>
+                        <div class="addCard">
+                            <div>
+                                <p><label for="task">Задача:</label></p>
+                                <input required id="task" v-model="taskChange" placeholder="Задача">
+                            </div>
+                            
+                            <div>
+                                <p><label for="description">Описание:</label></p>
+                                <textarea required id="description" v-model="descChange" placeholder="Описание"></textarea>
+                            </div>
+                            
+                            <div>
+                                <p><label  for="deadline">Дедалйн:</label></p>
+                                <input required id="deadline" v-model="dlChange" placeholder="Дедлайн" type="date">
+                            </div>
+                        </div>
+                        <input @click="changeModalCard" class="btn addBtn" type="submit" value="Добавить">
+                    </form>
+                </div>
+                
             </div>
             <div class="columns">
             
@@ -24,7 +49,13 @@ Vue.component('kanban', {
                         draggable
                         @dragstart="startDrag($event, card)"
                     >
-                        <card class="card" :card="card"></card>
+                        <card 
+                            class="card"
+                            :card="card"
+                            :changeModal="changeModal"
+                            :modal="modal"
+                            :changeModalCard="changeModalCard"
+                            :saveId="saveId"></card>
                     </div>
                 </div>
                 
@@ -46,69 +77,52 @@ Vue.component('kanban', {
                 
                 <div>
                     <h2>Выполненные</h2>
-                    <column :testingCards="testingCards"></column>
+                    <column></column>
                 </div>
                 
                 <div>
                     <h2>Выполненные</h2>
-                    <column :testingCards="testingCards"></column>
+                    <column></column>
                 </div>
             </div>
         </div>
     `,
     data() {
         return {
-            newCards: [
-                {
-                    id: 0,
-                    task: 243,
-                    description: 43242,
-                    deadline: 4234,
-                    date: new Date().toLocaleString(),
-                    completed: false,
-                    column: 1,
-                },
-                {
-                    id: 1,
-                    task: 243,
-                    description: 43242,
-                    deadline: 4234,
-                    date: new Date().toLocaleString(),
-                    completed: false,
-                    column: 1,
-                },
-                {
-                    id: 2,
-                    task: 243,
-                    description: 43242,
-                    deadline: 4234,
-                    date: new Date().toLocaleString(),
-                    completed: false,
-                    column: 2,
-                }
-            ],
-            inProgressCards: [],
-            testingCards: [],
-            completedCard: [],
-            addCard: false
+            allCards: [],
+            modal: false,
+            changeCard: false,
+            idChange: null,
+            taskChange: null,
+            descChange: null,
+            dlChange: null
         }
     },
     mounted() {
         eventBus.$on('addCard', card => {
-            this.newCards.push(card)
+            this.allCards.push(card)
         })
     },
     computed: {
         listOne() {
-            return this.newCards.filter((card) => card.column === 1)
+            return this.allCards.filter((card) => card.column === 1)
         },
         listTwo() {
-            return this.newCards.filter((card) => card.column === 2)
+            return this.allCards.filter((card) => card.column === 2)
         },
     },
     methods: {
         changeModal() {
-            this.addCard = !this.addCard
+            this.modal = !this.modal
+        },
+        changeModalCard() {
+            this.changeCard = !this.changeCard
+        },
+        saveId(card) {
+            this.idChange = card.id
+        },
+        updateCard(item) {
+            let card = this.allCards.find((item) => item.id === this.idChange)
         },
         startDrag(evt, card) {
             evt.dataTransfer.dropEffect = 'move'
@@ -117,7 +131,7 @@ Vue.component('kanban', {
         },
         onDrop(evt, column) {
             const cardID = evt.dataTransfer.getData('cardID')
-            const item = this.newCards.find((item) => item.id === cardID)
+            const item = this.allCards.find((item) => item.id === cardID)
             item.column = column
         },
     }
@@ -126,17 +140,27 @@ Vue.component('kanban', {
 Vue.component('card', {
     template: `
         <div>
-            <h2>{{ card.id }}</h2>  
             <h2>{{ card.task }}</h2>  
             <p>{{ card.description }}</p>
             <p>{{ card.deadline }}</p>
+            <input class="btn" type="button" value="Изменить" @click="updateCard" @click="changeModalCard" @click="saveId(card)">
         </div>
     `,
     props: {
         card: {
             type: Object
         },
-    }
+        changeModalCard: {
+            type: Function
+        },
+        saveId: {
+            type: Function
+        },
+        updateCard: {
+            type: Function
+        }
+    },
+
 })
 
 Vue.component('add-note', {
@@ -144,14 +168,22 @@ Vue.component('add-note', {
         <form @submit.prevent="sendCard">
             <h1 class="cross" @click="changeModal">+</h1>
             <div class="addCard">
-                <p><label for="task">Задача:</label>
-                    <input required id="task" v-model="task" placeholder="Задача"></p>
-                <p><label for="description">Описание:</label>
-                    <input required id="description" v-model="description" placeholder="Описание"></p>
-                <p><label  for="deadline">Дедалйн:</label>
-                    <input required id="deadline" v-model="deadline" placeholder="Дедлайн"></p>
+                <div>
+                    <p><label for="task">Задача:</label></p>
+                    <input required id="task" v-model="task" placeholder="Задача">
+                </div>
+                
+                <div>
+                    <p><label for="description">Описание:</label></p>
+                    <textarea required id="description" v-model="description" placeholder="Описание"></textarea>
+                </div>
+                
+                <div>
+                    <p><label  for="deadline">Дедалйн:</label></p>
+                    <input required id="deadline" v-model="deadline" placeholder="Дедлайн" type="date">
+                </div>
             </div>
-            <input @click="changeModal" class="btn" type="submit" value="Добавить">
+            <input @click="changeModal" class="btn addBtn" type="submit" value="Добавить">
         </form>
     `,
     props: {
